00cca6756d655a3af16c28353e000b1d587ba672
ronarmengol <ronarmengol1977@gmail.com>
2020-07-30 00:20:52 +0300

    added SEO in blog templates

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { Helmet } from 'react-helmet'
 
 
 export const query = graphql`
@@ -14,7 +15,6 @@ export const query = graphql`
                 json
             }
         }
-
     }
 `
 
@@ -33,11 +33,23 @@ const Blog = (props) => {
 
 
     return (
-        <Layout>    
-            <h1>{props.data.contentfulBlogPost.title}</h1>
-            <p>{props.data.contentfulBlogPost.publishedDate}</p>
-            {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
-        </Layout>
+        <div>
+        <Helmet>
+        <html lang="en" />
+            <title>{props.data.contentfulBlogPost.title}</title>
+            <meta name="author" content="Ron Armengol" />
+            <meta name="description" content={props.data.contentfulBlogPost.title} />
+            <meta name="keywords" content={props.data.contentfulBlogPost.title} />
+            <meta name="robots" content="index, follow" />
+            <meta name="google-site-verification" content="vkn_1OZWK2FO3owAlOm0UP4SM3KNYlCVR3vPi52Ue94" />
+        </Helmet>
+            <Layout>    
+                <h1>{props.data.contentfulBlogPost.title}</h1>
+                <p>{props.data.contentfulBlogPost.publishedDate}</p>
+                {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
+            </Layout>
+            
+        </div>
     )
 }
 
