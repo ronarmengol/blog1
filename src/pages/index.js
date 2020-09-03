a6dfc426aaa59da0da7c56fc3ed011e12cd0bd3a
Ron Armengol <ronarmengol1977@gmail.com>
2020-09-03 12:38:55 -0700

    emphasised and put strong on the blog keyword

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,9 +13,9 @@ const IndexPage = () => {
 
     <Helmet>
       <html lang="en" />
-      <title>Blogger freelance writer Kitwe Copperbelt Zambia blog </title>
+      <title>Blogger Zambia Copperbelt Kitwe | 2020 blog </title>
       <meta name="author" content="Ron Armengol" />
-      <meta name="description" content="Blogger, freelance blog writer Kitwe Copperbelt Zambia Ron Armengol home page introduction" />
+      <meta name="description" content="Blogger, Zambia, Copperbelt, Kitwe freelance blog writer | home page | Ron Armengol | introduction" />
       <meta name="keywords" content="Blog articles, freelance writer blogger Ron Armengol Kitwe Copperbelt Zambia" />
       <meta name="robots" content="index, follow" />
       <meta name="google-site-verification" content="vkn_1OZWK2FO3owAlOm0UP4SM3KNYlCVR3vPi52Ue94" />
@@ -35,14 +35,14 @@ const IndexPage = () => {
       <p style={{fontSize: `3rem`}}>Hi, I am Ron,</p>
       <h1>web developer (designer) in Zambia</h1>
       <p>
-        I have now learned enough to open up my own website, and store content in a cloud database CMS. I am an aspiring to be a better writer. I did not want to start blogging when I felt good enough to write, I know this would lead to gross procrastination, I think with any grand plans, we should just dive in, and learn as we go along.
+        I have now learned enough to open up my own website, and store content in a cloud database CMS. I am an aspiring to be a better writer. I did not want to start <em><strong>blogging</strong></em> when I felt good enough to write, I know would have ended up never starting, I think with any grand plans, we should just dive in, and learn as we go along.
         <br></br>
-        Below, I hope to share all the tools and lessons I learn in this new and exciting journey of blogging and programming, and in the blog page I will basically pen down random thoughts and ideas.
+        Below, I hope to share all the tools and lessons I learn in this new and exciting journey of <em><strong>blogging</strong></em> and programming, and in the <em><strong>blog`</strong></em> page I will basically pen down random thoughts and ideas.
         <br></br>
-        I am extremely fortunate that the Covid 19 restrictions have not hindered my pursuit of programming. I am currently in Kitwe, <em>Zambia</em>, writing this blog. I love it here in Kitwe, its a small city, and little traffic. Generally in the Copperbelt it is quiet compared to the capital Lusaka.
+        I am extremely fortunate that the Covid 19 restrictions have not hindered my pursuit of programming. I am currently in <em>Kitwe</em>, <em>Zambia</em>, writing this <em><strong>blog</strong></em>. I love it here in Kitwe, its a small city, and little traffic. Generally in the <em>Copperbelt</em> it is quiet compared to the capital Lusaka.
       </p>
       <p>
-        I am not using the Wordpress platform or any other website builder. I am using HTML, CSS and Javascript for other websites I build. But for this blog I am using React in the Gatsby platform with the Contentful CMS. I am planning to improve my React skills once I buy a much faster computer. The development build is a crazy 5 minute wait.
+        I am not using the Wordpress platform or any other website builder. I am using HTML, CSS and Javascript for other websites I build. But for this <em><strong>blog</strong></em> I am using React in the Gatsby platform with the Contentful CMS. I am planning to improve my React skills once I buy a much faster computer. The development build is a crazy 5 minute wait.
       </p>
       <p>
         If you are in Kitwe, Ndola, Lusaka or anywhere in Zambia and you are seriously considering thinking of programming or web development, you do not need to go out of the country. There is enough resources on the internet to learn. 
@@ -52,7 +52,7 @@ const IndexPage = () => {
 
     <section className={layoutStyles.container2}>
         <h2>Writing tools</h2>
-        <h5>Install grammarly, the best blog tool.</h5>
+        <h5>Install grammarly, the best <em><strong>blog</strong></em> tool.</h5>
         <p>Poor grammar and bad spelling will definitely make any writer look amateur. Grammarly is amazing it assists in checking your spelling, grammar and punctuation. And reminds you if sentences are too windy.</p>
         <h5>Thesaurus and Dictionary</h5>
         <p>Do not get mixed up with similar sounding words like inspire, and aspire, double check you have used the right word to express your content. Spice up your content with variety of synonyms with an online Thesaurus. </p>
@@ -117,32 +117,63 @@ const IndexPage = () => {
         The programmer solves by using a grid layout and media queries breakpoints. This makes the layout of the webpage dynamic. In simple terms this is moving content that is stacked horizontally and making them stack vertically as the screen width reaches certain breakpoints (specified pixel widths).</p>
         <p>If you are a beginner, containerize (or wrap) your content in a div that has a max-width of say 600px that is text centered on the screen. Here you do not need to worry about columns changing in a smaller viewport. It is important to reduce the text width because it is tiring for readers to read text that compasses the full width of the laptop or desktop screen. </p>
         <h5>Images</h5>
-        <p>Responsive images is the concern of images reacting to the viewport. Images naturally resize to the parent container (html div tag) unless we put a fixed width and height to the image. This is not pure responsiveness. My laptop pixel dimension is about 1350 by 6</p>
+        <p>Responsive images is the concern of images reacting to the viewport. Images naturally resize to the parent container (html div tag) unless we put a fixed width and height to the image. This is not pure responsiveness. My laptop pixel dimension is about 1350 by 650</p>
 
+        
 
         <h2 style={{marginTop: `2rem`, marginBottom: `1rem`}}>Miscellaneous</h2>
         <h5>Timeline</h5>
-          14th July 2020 - posted my first post <br />
-          28th July 2020 - applied SEO on my blog <br />
+        <p>
+          14th July 2020 - posted my first <em><strong>blog</strong></em> post <br />
+          28th July 2020 - applied SEO on my <em><strong>blog</strong></em>. <br />
           29th July 2020 - ranked 32 with Kitwe Bloggers keywords <br />
           8th August 2020 - ranked 4 with Kitwe Bloggers keywords <br />
           18th August 2020 - I did not appear at all for keywords Zambia blogger. Inserted some SEO for Zambia. <br />
-          20th August 2020 - ranked 3 with Kitwe Blogger keywords and ranked 3 with Copperbelt blogger. <br />
-          20th August 2020 - ranked 136 with Zambia Blogger keywords and ranked 3 with Copperbelt blogger.<br />
-          2nd September 2020 - ranked 1 with Kitwe Blogger and or blogger Kitwe <br />
+          20th August 2020 - ranked 3 with <em>Kitwe blogger</em> keywords and ranked 3 with <em>Copperbelt blogger</em>. <br />
+          20th August 2020 - ranked 136 with Zambia Blogger keywords and ranked 3 with <em>Copperbelt blogger</em><br />
           2nd September 2020 - ranked 1 with Copperbelt blogger or Copperbelt blogger <br />
-          2nd September 2020 - ranked 20 with blogger zambia and 46 with zambia blogger
+          2nd September 2020 - ranked 1 with Kitwe Blogger and or blogger Kitwe <br />
+          2nd September 2020 - ranked 20 with blogger zambia and 46 with zambia blogger <br />
 
+        </p>
 
+        
+        <p>
+          I have kept the time line above to show the effect of SEO to the <em><strong>blog</strong></em> post. It is important to note that even though your SEO is good, it still takes time to rank above a website whose SEO score is lower than yours.
+        </p>
+        <p>
+          What is also very important to note that the google searcher can also affect the results by the order he puts the keywords into the search engine as demonstrated in the Timeline for 2nd September 2020. I am not sure why this happens. Definitely need more research and I will add to this post later on.
+        </p>
+        <p>
+          Another issue I must raise, is that over time this <em><strong>blog</strong></em> page has increased in content. I am certain the increase in content has a positive effect over the SEO for the <em>blogging</em>.
+        </p>
+        <p>
+          I must also give a little credit to my SEO skills over the last couple of months. I think basic on page SEO is easy to understand. Unfortunately implementation is really quite tedious and boring. I feel people who do not apply SEO are completely ignorant of it and its importance or, just cannot be bothered to apply it.
+        </p>
 
         <h2 style={{marginTop: `2rem`, marginBottom: `1rem`}}>Technicals</h2>
         <h6>The boring geeky stuff</h6>
-        <p>The first week of August I have been struggling to work on website speed. I don't work on my blog speed for my blog site because it runs under a Gatsby framework which is renowned for speed</p>
+        <p>The first week of August I have been struggling to work on website speed. I don't work on my <em><strong>blog</strong></em> speed for my <em><strong>blog</strong></em> site because it runs under a Gatsby framework which is renowned for speed</p>
         <p>The real issue is jquery if it is deferred, it makes the javascript in the page sleep. The drop down menu bars stop functioning, and jquery is quite heavy on code. If it is any consolation, Bootrap 5 is coming out, it will not be using JQuery in its library. And another big one is it will no longer support Explorer IE brower.</p>
         <p>When I look at come of the themes available too many are using JQuery. If JQuery is removed I seem to see the download shave another 1 - 1.5 seconds.</p>
+        <p>
+          It is important to note I have not added any structured data for the website SEO. I will do this at a later date. 
+        </p>
         <p>Bought a new lenovo computer 500 GB static hard drive and 16GB RAM. Absolutely fantastic. With my 4GB HP it would take over 5 minutes to prepare the gatsby develop, with the Lenovo 20 seconds. When I used Gatsby with the HP, it was so slow that even VS code would lag, other programs stopped. The 16GB lenovo is an absolute powerhouse.</p>
+
+        <h2 style={{marginTop: `2rem`, marginBottom: `1rem`}}>Future plans</h2>
+        <p>
+          September - October 2020: I want to work on CSS and website design. Website design does not require much Javascript. Javascript used for simple animations are usually provided online as code snippets.
+        </p>
+        <p>
+          November - December 2020: I want finish a javascript crash course and follow a couple of online javascript projects.
+        </p>
+        <p>
+          January - May 2021: Study and practise React on the Gatsby Platform. In this period I will prettify this <em><strong>blog</strong></em> site. Apologies to my viewers. My current <em>React</em> skills are still dismal. I am not keen to start learning it until I have mastered Javascript.
+        </p>
     </section>
     
+
 
       <div style={{textAlign: `center`}}>
       <Footer />
